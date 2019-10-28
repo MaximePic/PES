ab0979c85c46473989a1b8f5e58d343863cb2a02
MaximePic <maxime.pic@hotmail.fr>
2019-10-28 14:12:51 +0100

    Update euler003.js

--- euler003.js
+++ euler003.js
@@ -1,14 +1,14 @@
 (function euler003(){
-calcLargestPrimeFactor(600851475143);
-function calcLargestPrimeFactor(n){
-	var i = 2;
-	while(i<=n){
-		if(n%i == 0){
-			n/=i;
-		} else {
-			i++;
+	calcLargestPrimeFactor(600851475143);
+	function calcLargestPrimeFactor(n){
+		var i = 2;
+		while(i<=n){
+			if(n%i == 0){
+				n/=i;
+			} else {
+				i++;
+			}
 		}
+		console.log(i);
 	}
-	console.log(i);
-}
 }());
